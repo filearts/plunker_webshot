c8dbaf9d0c1342ef377dbd56919fe44b845a8469
Geoff Goodman <ggoodman@gmail.com>
2015-07-22 12:33:23 -0400

    Minor fixes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var Image = require("imagemagick-stream");
 var Joi = require("joi");
 var LRU = require("bluebird-lru-cache");
 var Promise = require("bluebird");
-var Webshot = require("webshot");
+var Screenshot = require("screenshot-stream");
 
 
 if (!process.env.RUN_URL) throw new Error("RUN_URL environment variable is required.");
@@ -14,52 +14,35 @@ if (!process.env.PORT) throw new Error("PORT environment variable is required.")
 var server = new Hapi.Server(process.env.HOST || "0.0.0.0", process.env.PORT);
 
 
+setTimeout(function () {
+  server.log('info', 'Server exceeded maximum lifetime, exiting.');
+  process.exit(0);
+}, 1000 * 60 * 60);
+
+
 var internals = {};
 
 internals.runUrl = process.env.RUN_URL;
 
-internals.capture = function (url, config) {
-  return new Promise(function (resolve, reject) {
-    Webshot(url, config, function (err, readStream) {
-      if (err) return reject(err);
-      
-      return resolve(readStream);
-    });
-  });
-};
-
 internals.prepareShot = function (key) {
   var plunkId = key.split("@")[0];
-  var params = {
-    errorIfStatusIsNot200: true,
-    renderDelay: 3,
-    timeout: 1000 * 10,
-    screenSize: {
-      width: 1024,
-      height: 768,
-    },
-    shotSize: {
-      width: 1024,
-      height: "all",
-    },
-  };
   
-  return internals.capture(internals.runUrl + "/plunks/" + plunkId + "/", params)
-    .then(function (renderStream) {
-      return new Promise(function (resolve, reject) {
-        var resizeStream = Image().resize("248").gravity("NorthWest").crop("248x372").quality(75);
-        var concatStream = Concat(function (buf) {
-          if (!buf.length) return reject(Boom.serverTimeout("Invalid preview, empty buffer"));
-          
-          resolve(buf);
-        });
-        
-        renderStream
-          .pipe(resizeStream)
-          .pipe(concatStream)
-          .on("error", reject);
-      });
+  return new Promise(function (resolve, reject) {
+    var captureStream = Screenshot(internals.runUrl + "/plunks/" + plunkId + "/", "1024x768", {delay: 2});
+    var resizeStream = Image().resize("248").gravity("NorthWest").crop("248x372").quality(75);
+    var concatStream = Concat(function (buf) {
+      if (!buf.length) {
+        return reject(Boom.serverTimeout("Invalid preview, empty buffer"));
+      }
+      
+      resolve(buf);
     });
+    
+    captureStream
+      .pipe(resizeStream)
+      .pipe(concatStream)
+      .on("error", reject);
+  });
 };
 
 
